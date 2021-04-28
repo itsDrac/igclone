d32cc9ad6767acec2df7cde8ed8a9fb4e23122aa
Sahaj <gpt.sahaj28@gmail.com>
2021-04-28 20:35:40 +0530

    01 Try to fix CSRF Error in heroku

--- app/extinsions.py
+++ app/extinsions.py
@@ -3,7 +3,7 @@ from flask_migrate import Migrate
 from flask_login import LoginManager
 from flask_mail import Mail
 from flask_dropzone import Dropzone
-from flask_wtf.csrf import CSRFProtect
+#from flask_wtf.csrf import CSRFProtect
 
 db = SQLAlchemy()
 
@@ -18,4 +18,4 @@ mail = Mail()
 
 dropzone = Dropzone()
 
-csrf = CSRFProtect()
+#csrf = CSRFProtect()
