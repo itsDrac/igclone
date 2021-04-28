f36e4c14b7117d0af7469cc583934282422fabf9
Sahaj <gpt.sahaj28@gmail.com>
2021-04-28 16:23:49 +0530

    Updated requirements.txt

--- app/__init__.py
+++ app/__init__.py
@@ -2,10 +2,10 @@ from flask import Flask
 from app.extinsions import db, migrate, login_manager, mail, dropzone, csrf
 from app.commands import create_tables
 from app.user import user
-from app.user.models import User
+#from app.user.models import User
 from app.main import main
 from app.post import post
-import flask_whooshalchemy as wa
+#import flask_whooshalchemy as wa
 
 def create_app():
     app = Flask(__name__)
@@ -17,7 +17,7 @@ def create_app():
     mail.init_app(app)
     dropzone.init_app(app)
     csrf.init_app(app)
-    wa.whoosh_index(app, User)
+#    wa.whoosh_index(app, User)
 
     app.register_blueprint(user, url_prefix='/user')
     app.register_blueprint(post, url_prefix='/post')
